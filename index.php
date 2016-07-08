72cfe7862e5735699ddbefb61bf2010ff56897ff
Jafferwaffer <thomas.ge@hotmail.co.uk>
2016-07-08 13:59:07 +0100

    Added imgs

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
 		<script src="libs\jssor.slider.min.js"></script>
 	</head>
 	<body>
+		<div id="slider_container" style="position:relative; top:0px; left:0px; width:600px; height:300px;">
+			<!-- Slides Container -->
+			<div u="slides" style="cursor: move; position: absolute; overflow:hidden; left:0px; top:0px; width:600px;height:300px;">
+				<div><img u="image" src=""/></div>
+				<div><img u="image" src=""/></div>
+				<div><img u="image" src=""/></div>
+				<div><img u="image" src=""/></div>
+			</div>
+		</div>
 		<script>
 			jQuery(document).ready(function($){
 				var options = {$AutoPlay: true};
