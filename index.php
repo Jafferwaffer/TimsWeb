9dd4f5922e0a1f92435af0327ea26147acd2b2b4
Jafferwaffer <thomas.ge@hotmail.co.uk>
2016-07-08 13:45:28 +0100

    Slider function

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
 		<script src="libs\jssor.slider.min.js"></script>
 	</head>
 	<body>
+		<script>
+			jQuery(document).ready(function($){
+				var options = {$AutoPlay: true};
+				var jssor_slider_home = new  $JssorSlider$('slider_container',options);
+			});
+		</script>
 	</body>
 </html>
