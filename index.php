7a27189927b03c928b65e68624416cdd754816eb
fullmuffin <alex@alexclarke.me.uk>
2016-07-08 14:08:09 +0100

    added css

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-		<script src="libs\jssor.slider.min.js"></script>
 	</head>
 	<body>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>Prices</li>
+				<li>About</li>
+				<li id="end">Contact</li>
+			</ul>
+		</nav>
+		<main>
+			<!--slider here-->
+		</main>
 	</body>
 </html>
