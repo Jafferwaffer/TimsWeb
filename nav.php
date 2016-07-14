3cd6476bd6ca4d187cc2caa791b5de2fe30c913a
Jafferwaffer <thomas.ge@hotmail.co.uk>
2016-07-14 12:08:21 +0100

    Started work on portfolio - added gallery

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 		<nav>
 			<ul>
 				<a href="index.php"><li>Home</li></a>
-				<a href="#"><li>Portfolio</li></a>
+				<a href="portfolio.php"><li>Portfolio</li></a>
 				<a href="#"><li>Prices</li></a>
 				<a href="#"><li>About</li></a>
 				<a href="contact.php"><li id="end">Contact</li></a>
---
+++ portfolio.php
@@ -0,0 +1,40 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/gallery.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<script src="js\currentPage.js"></script>	
+	</head>
+	<body>
+		<?php include 'nav.php'; ?>
+		<section class="gallery">
+			<div class="img">
+			  <a target="_blank" href="img/mountain_1.jpg">
+				<img src="img/mountain_1.jpg" alt="Fjords" width="300" height="200">
+			  </a>
+			  <div class="desc">Add a description of the image here</div>
+			</div>
+
+			<div class="img">
+			  <a target="_blank" href="forest.jpg">
+				<img src="img/mountain_1.jpg" alt="Forest" width="300" height="200">
+			  </a>
+			  <div class="desc">Add a description of the image here</div>
+			</div>
+
+			<div class="img">
+			  <a target="_blank" href="lights.jpg">
+				<img src="img/mountain_1.jpg" alt="Northern Lights" width="300" height="200">
+			  </a>
+			  <div class="desc">Add a description of the image here</div>
+			</div>
+
+			<div class="img">
+			  <a target="_blank" href="mountains.jpg">
+				<img src="img/mountain_1.jpg" alt="Mountains" width="300" height="200">
+			  </a>
+			  <div class="desc">Add a description of the image here</div>
+			</div>
+		</section>
+	</body>
+</html>
